6f8b58adce35b06535b3e9bbb8aff68f10dc3fd9
DaniCas <daniel.castrillon.mt@correo.usa.edu.co>
2021-10-27 22:21:51 -0500

    Subir cambios vista

--- src/main/java/com/danielcastrillon/motoG11/reports/CountClients.java
+++ src/main/java/com/danielcastrillon/motoG11/reports/CountClients.java
@@ -20,7 +20,33 @@ public class CountClients {
         this.total = total;
         this.client = client;
     }
-    
-    
+
+    /**
+     * @return the total
+     */
+    public Long getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the client
+     */
+    public Client getClient() {
+        return client;
+    }
+
+    /**
+     * @param client the client to set
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }  
     
 }
